6e26e60b378b8cdf5e2a1355d040c10f73a908cc
MostafaOwies <mostafaowies@yahoo.com>
2021-07-25 17:45:02 +0200

    commit 1

--- app/src/main/java/com/example/numbershapes/MainActivity.java
+++ app/src/main/java/com/example/numbershapes/MainActivity.java
@@ -3,8 +3,22 @@ package com.example.numbershapes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    class number{
+        int numb ;
+
+
+    }
+
+    public void shapenumb(View view){
+
+        Log.i("test","test");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
